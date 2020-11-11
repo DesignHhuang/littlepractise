e4c37a0ca7b4b2b5024449f910c8c2a82966e78e
unknown <1219549841@1qq.com>
2020-11-11 12:16:43 +0800

    3231313

--- 1.js
+++ 1.js
@@ -31,7 +31,7 @@ console.log(result)
 
 
 
-var arr = [11, 1, 0, 2, 3, 5];
+let arr = [11, 1, 0, 2, 3, 5];
 // 取最大
 var max1 = Math.max(...arr);
 var max2 = Math.max(...arr);
@@ -43,3 +43,9 @@ console.log(max1); //11
 console.log(max2); //11
 console.log(min1); //0
 console.log(min2); //0
+
+var fn = function () {
+  var arr = [...arguments];
+  console.log(arr); //[1, 2, 3, 4]
+};
+fn(1, 2, 3, 4);
